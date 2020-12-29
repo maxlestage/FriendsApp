f82864df4f31a94dd0290e8208f9ba3fa27f0c4e
Max <maxlestage@icloud.com>
2020-12-29 10:15:43 +0100

    prod port 465

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -123,10 +123,10 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port: 587,
+    port: 465,
     domain: "gmail.com",
     authentication: "plain",
-    enable_starttls_auto: false,
+    enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password:ENV["GMAIL_PASSWORD"]
   }
