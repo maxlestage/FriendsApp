0ef36d2e2d6f7b6eae2cc75209112d61fa311a61
Max <maxlestage@icloud.com>
2020-12-29 10:22:32 +0100

    comment   enable_starttls_auto: true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -126,7 +126,7 @@ Rails.application.configure do
     port: 465,
     domain: "gmail.com",
     authentication: "plain",
-    enable_starttls_auto: true,
+    # enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password:ENV["GMAIL_PASSWORD"]
   }
